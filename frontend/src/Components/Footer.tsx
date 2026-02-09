72b7befd6a31983ff7b8abf756c7a0b6c9486849
Devindin <devindikarunathilaka88@gmail.com>
2026-02-09 11:43:36 +0530

    add auth background to forgot password page

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(): React.JSX.Element {
   return (
     <div className="text-center text-xs text-[#1976D2] dark:text-gray-400">
-      <p>&copy; 2024 <span className="text-[#00C6D7] font-semibold">IssueDesk</span><span className="text-[#0A3D91] font-bold">X</span>. All rights reserved.</p>
+      <p>&copy; 2026 <span className="text-[#00C6D7] font-semibold">IssueDesk</span><span className="text-[#0A3D91] font-bold">X</span>. All rights reserved.</p>
     </div>
   );
 }
