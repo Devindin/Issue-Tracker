a875f6e2649ac5eb0e58f8e433bda96a79421c90
Devindin <devindikarunathilaka88@gmail.com>
2026-02-09 14:16:05 +0530

    create authmiddleware

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -6,7 +6,6 @@ const authMiddleware = (req, res, next) => {
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.jwt_secret);
     req.user = decoded;
